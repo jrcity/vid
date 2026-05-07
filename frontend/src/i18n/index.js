6882ff4ccd2df4729dcea9e4c9a904079b3f1c05
ThamieX <134184304+Zandruh@users.noreply.github.com>
2026-05-06 18:10:24 -0700

    Create index.js
    African LLMS model index i18n file

---
+++ frontend/src/i18n/index.js
@@ -0,0 +1,19 @@
+import en from './en.json';
+import sw from './sw.json';
+import ha from './ha.json';
+import tw from './tw.json';
+import zu from './zu.json';
+import am from './am.json';
+import st from './st.json';  // Sesotho
+
+const translations = {
+  en,
+  sw,
+  ha,
+  tw,
+  zu,
+  am,
+  st  // Sesotho added
+};
+
+export default translations;
